4b81f09a92fbc2d6707a4883fa5ee1d686993104
최방실 <qkdtlf89089@gmail.com>
2020-02-29 23:27:23 +0900

    add discountList/selector/renderList component

---
+++ src/component/renderList/selector/DiscountList.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import DiscountCard from "../../renderItems/selector/DiscountCard";
+import { Discount } from "../../../types";
+
+interface IProps {
+  discounts: Discount;
+}
+export default function ItemList({ discounts }: IProps) {
+  const renderArray = Object.keys(discounts).map(key => ({
+    [key]: discounts[key]
+  }));
+
+  return (
+    <div className="selector__render-list-container">
+      {renderArray.map((discount, i) => (
+        <DiscountCard key={i} discount={discount} />
+      ))}
+    </div>
+  );
+}
